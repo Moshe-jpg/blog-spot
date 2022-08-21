f3a300f4a72010b58f3d680bc7278b1b5a8556c6
Moshe <mobgadol@gmail.com>
2022-08-21 18:27:51 -0400

    modified seeds and added helpers

--- seeds/index.js
+++ seeds/index.js
@@ -1,22 +1,20 @@
-const seedUsers = require('./user-seeds');
-const seedQuotes = require('./posts-seeds');
-const seedComments = require('./comment-seeds');
+const seedUsers = require("./user-seeds");
+const seedPosts = require("./posts-seeds");
+const seedComments = require("./comment-seeds");
 
-
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('--------------');
+  console.log("--------------");
   await seedUsers();
-  console.log('--------------');
+  console.log("--------------");
 
-  await seedQuotes();
-  console.log('--------------');
+  await seedPosts();
+  console.log("--------------");
 
-//   await seedComments();
-  console.log('--------------');
-
+  await seedComments();
+  console.log("--------------");
 
   process.exit(0);
 };
